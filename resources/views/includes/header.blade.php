664738e8f3ef219365f3f687eb53cb613cf72df4
Pedro Lugo <robertty55@gmail.com>
2016-05-17 22:44:37 -0430

    Creacion de menu faltante, TOP DE ESTUDIANTES

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -14,6 +14,7 @@
             @if(Auth::check())
                 <li><a href="{{route('usuarioAsignaturas')}}">Inscribir Asignatura</a></li>
                 <li><a href="{{route('preguntas')}}">Preguntas</a></li>
+                <li><a href="{{route('top')}}">Top Estudiantes</a></li>
                 @if(Auth::user()->tipo_usuario_id==1)
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Administrar <span class="caret"></span></a>
